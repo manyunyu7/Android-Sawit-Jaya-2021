ac87d871f3efabe3d86840bfd647cd22a628fd6f
henryaugusta <henryaugusta4@gmail.com>
2021-08-30 22:29:49 +0700

    🗃🏡💻 create room database for local storage

---
+++ app/src/main/java/com/feylabs/sawitjaya/data/local/room/MyRoomDatabase.kt
@@ -0,0 +1,37 @@
+package com.feylabs.sawitjaya.data.local.room
+
+import android.content.Context
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import com.feylabs.sawitjaya.data.local.room.dao.AuthDAO
+import com.feylabs.sawitjaya.data.local.room.entity.AuthEntity
+
+@Database(
+    entities = [
+        AuthEntity::class],
+    version = 4
+)
+abstract class MyRoomDatabase : RoomDatabase() {
+
+    abstract fun authDao() : AuthDAO
+
+    companion object {
+        const val DB_NAME = "soho_medrep.db"
+
+        @Volatile
+        private var instance: MyRoomDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                MyRoomDatabase::class.java, "soho_medrep.db"
+            ).allowMainThreadQueries()
+                .build()
+
+    }
+}
