63a75ef7cef05089403d7cea8030be1d56878fa6
henryaugusta <henryaugusta4@gmail.com>
2021-08-30 23:16:37 +0700

    🌐 Change Base URL into local development server

--- app/src/main/java/com/feylabs/sawitjaya/service/ApiService.kt
+++ app/src/main/java/com/feylabs/sawitjaya/service/ApiService.kt
@@ -1,4 +1,47 @@
 package com.feylabs.sawitjaya.service
 
-interface ApiService {
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiService {
+
+    fun getClient(context: Context): ApiService {
+
+        val baseURL = "http://192.168.1.161:3202/"
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor(
+                ChuckerInterceptor.Builder(context)
+                    .collector(ChuckerCollector(context))
+                    .maxContentLength(250000L)
+                    .redactHeaders(emptySet())
+                    .alwaysReadResponseBody(true)
+                    .build()
+            )
+            .build()
+
+        val gson = GsonBuilder().serializeNulls().disableHtmlEscaping().create()
+
+        val retrofitz = Retrofit.Builder()
+            .baseUrl(baseURL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+
+        return retrofitz.create(ApiService::class.java)
+
+    }
+
 }
