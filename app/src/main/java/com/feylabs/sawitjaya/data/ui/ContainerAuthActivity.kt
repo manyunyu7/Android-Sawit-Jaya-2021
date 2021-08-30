a3ed6e2c74c17cce9d6d212b6273053f910f2daa
henryaugusta <henryaugusta4@gmail.com>
2021-08-30 23:43:23 +0700

    🌈 🧭️ Add Container for Jetpack Navigation on Auth, refactor MainActivity.kt to UI Folder

---
+++ app/src/main/java/com/feylabs/sawitjaya/data/ui/ContainerAuthActivity.kt
@@ -0,0 +1,23 @@
+package com.feylabs.sawitjaya.data.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.feylabs.sawitjaya.R
+import com.feylabs.sawitjaya.databinding.ActivityContainerAuthBinding
+
+class ContainerAuthActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityContainerAuthBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        actionBar?.hide()
+        val navController = findNavController(R.id.nav_host_auth)
+        setupActionBarWithNavController(navController)
+    }
+}
--- app/src/main/java/com/feylabs/sawitjaya/data/ui/MainActivity.kt
+++ app/src/main/java/com/feylabs/sawitjaya/data/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.feylabs.sawitjaya
+package com.feylabs.sawitjaya.data.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feylabs.sawitjaya.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
