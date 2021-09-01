0c04592a214a9d679f2d56f7fdd3f8fc053166e1
henryaugusta <henryaugusta4@gmail.com>
2021-09-01 20:47:36 +0700

    update main endpoint, and RemoteDataSource.kt into new endpoint

--- app/src/main/java/com/feylabs/sawitjaya/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/feylabs/sawitjaya/data/remote/RemoteDataSource.kt
@@ -2,17 +2,39 @@ package com.feylabs.sawitjaya.data.remote
 
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.feylabs.sawitjaya.data.local.preference.MyPreference
+import com.feylabs.sawitjaya.data.remote.request.RegisterRequestBody
+import com.feylabs.sawitjaya.data.remote.response.ChangePasswordResponse
 import com.feylabs.sawitjaya.data.remote.response.LoginResponse
+import com.feylabs.sawitjaya.data.remote.response.NewsResponse
+import com.feylabs.sawitjaya.data.remote.response.UserUpdateProfileResponse
+import com.feylabs.sawitjaya.service.ApiService
 import com.feylabs.sawitjaya.service.MainEndpoint
 import com.feylabs.sawitjaya.service.Resource
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import okhttp3.ResponseBody
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Response
+import timber.log.Timber
+import java.io.File
+import java.lang.Exception
 import retrofit2.Callback as retrocallbak
 
 class RemoteDataSource(
     private val api: MainEndpoint,
+    private val context: Context
 ) {
 
+    private var token: String? = ""
+
+    init {
+        token = MyPreference(context).getPrefString("TOKEN")
+    }
 
     /*
     Login
@@ -43,9 +65,194 @@ class RemoteDataSource(
                 returnValue.postValue(Resource.Error("Terjadi Kesalahan"))
             }
         })
-
         return returnValue
+    }
+
+    /**
+    Login
+    @param username,password
+     */
+    fun updateProfile(
+        name: String,
+        email: String,
+        contact: String,
+        role: String,
+        callback: CallbackUpdateProfile
+    ) {
+        callback.value(Resource.Loading())
+        api.update_data(
+            token.toString(),
+            email, role, name, contact,
+        ).enqueue(object : retrocallbak<UserUpdateProfileResponse> {
+            override fun onResponse(
+                call: Call<UserUpdateProfileResponse>,
+                response: Response<UserUpdateProfileResponse>
+            ) {
+                Timber.d("rds update_data ${response.raw()}")
+                val messageID = response.body()?.messageId
+                val messageEN = response.body()?.messageEn
+                if (response.body()?.statusCode == 1) {
+                    callback.value(Resource.Success(response.body(), messageID.toString()))
+                } else {
+                    callback.value(Resource.Error(response.body()?.messageId.toString()))
+                }
+            }
+
+            override fun onFailure(call: Call<UserUpdateProfileResponse>, t: Throwable) {
+                Timber.d("rds update_data : onFailure")
+                callback.value(Resource.Error("Terjadi Kesalahan"))
+            }
+        })
+    }
+
+
+    /**
+     * Register
+     * @param body,callback
+     *
+     */
+    fun register(
+        body: RegisterRequestBody,
+        callback: CallbackRegisterProfile
+    ) {
+        callback.value(Resource.Loading())
+        api.register(token, body)?.enqueue(object : retrocallbak<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                Timber.d("response register (success) -> ${response.body()}")
+                try {
+                    val json = JSONObject(response.body()!!.string())
+                    val statusCode: Int? = json?.getInt("status_code")
+                    val message: String = json?.getString("message") ?: "";
+                    Timber.d("response register status_code (success) -> ${statusCode}")
+                    Timber.d("response register message (success) -> ${message}")
+                    if (statusCode == 1) {
+                        callback.value(Resource.Success("Registrasi Berhasil"))
+                    } else {
+                        callback.value(Resource.Error(message))
+                    }
+                } catch (e: Exception) {
+                    Timber.d("raz exception at RemoteDataSource @register -> ${e.toString()}")
+                    callback.value(Resource.Error("Registrasi Gagal"))
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                callback.value(Resource.Success("Registrasi Gagal"))
+            }
+        })
+    }
+
+
+    /**
+     * Register
+     * @param body,callback
+     *
+     */
+    fun changePassword(
+        old_password: String,
+        new_password: String,
+        callback: CallbackChangePasswordProfile
+    ) {
+        callback.value(Resource.Loading())
+
+        api.changePassword(token, old_password, new_password)
+            ?.enqueue(object : retrocallbak<ChangePasswordResponse> {
+                override fun onResponse(
+                    call: Call<ChangePasswordResponse>,
+                    response: Response<ChangePasswordResponse>
+                ) {
+                    if (response.isSuccessful && response.body()?.statusCode == 1) {
+                        callback.value(Resource.Success(response.body()))
+                    } else {
+                        callback.value(Resource.Error(response.body()?.messageId.toString()))
+                    }
+                }
+
+                override fun onFailure(call: Call<ChangePasswordResponse>, t: Throwable) {
+                    callback.value(Resource.Error("Error : " + t.localizedMessage.toString() + " " + t.message))
+                }
+            })
+    }
+
+
+    /**
+     * Register
+     * @param body,callback
+     *
+     */
+    suspend fun getNews() =
+        api.getNews(token)
+
+
+    /**
+     * Register
+     * @param body,callback
+     *
+     */
+    fun changeProfiePicture(
+        file: File,
+        callback: CallbackChangeProfilePicture,
+    ) {
+        AndroidNetworking.upload(
+            ApiService.baseURL + "user/update-photo"
+        )
+            .addMultipartFile("photo", file)
+            .addHeaders("Authorization", token)
+            .build()
+            .setUploadProgressListener { bytesUploaded, totalBytes ->
+                val value = "${bytesUploaded / 1024} KB of ${totalBytes / 1024} KB"
+                callback.value(Resource.Loading(value))
+            }
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject?) {
+                    Timber.d("FAN Upload Success : $response")
+                    val res = response
+                    val status_code = res?.getInt("status_code") ?: 0
+                    val message = res?.getString("message_id") ?: "Default Message"
+                    if (status_code == 1) {
+                        callback.value(Resource.Success(message, message))
+                    } else {
+                        //IF UPLOAD FILE IS FAILED
+                        //Show the reason
+                        Timber.d("FAN Upload Error : response statcode")
+                        callback.value(Resource.Error(message, message))
+                    }
+                }
+
+                override fun onError(anError: ANError?) {
+                    Timber.d("FAN Upload Error : $anError")
+                    Timber.d("FAN Upload Error Body : ${anError?.errorBody}")
+                    Timber.d("FAN Upload Error Code : ${anError?.errorCode}")
+                    Timber.d("FAN Upload Error Detail : ${anError?.errorDetail}")
+                    Timber.d("FAN Upload Error Localized Message : ${anError?.localizedMessage}")
+                    callback.value(Resource.Error("Error : " + anError))
+                }
+
+            })
+
 
     }
+
+
+    interface CallbackUpdateProfile {
+        fun value(response: Resource<UserUpdateProfileResponse?>)
+    }
+
+    interface CallbackRegisterProfile {
+        fun value(response: Resource<String?>)
+    }
+
+    interface CallbackChangePasswordProfile {
+        fun value(response: Resource<ChangePasswordResponse?>)
+    }
+
+    interface CallbackChangeProfilePicture {
+        fun value(response: Resource<String?>)
+    }
+
+    interface CallbackNews {
+        fun value(response: Resource<NewsResponse?>)
+    }
+
 
 }
--- app/src/main/java/com/feylabs/sawitjaya/service/MainEndpoint.kt
+++ app/src/main/java/com/feylabs/sawitjaya/service/MainEndpoint.kt
@@ -1,12 +1,12 @@
 package com.feylabs.sawitjaya.service
 
-import com.feylabs.sawitjaya.data.remote.response.LoginResponse
+import com.feylabs.sawitjaya.data.remote.request.RegisterRequestBody
+import com.feylabs.sawitjaya.data.remote.response.*
+import okhttp3.ResponseBody
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Response
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface MainEndpoint {
 
@@ -21,8 +21,68 @@ interface MainEndpoint {
 
     @FormUrlEncoded
     @POST("auth/login")
-     fun login(
+    fun login(
         @Field("email") username: String,
         @Field("password") password: String
     ): Call<LoginResponse>
+
+    /**
+     * register
+     * @param get new profile
+     * @return the token
+     */
+    @POST("auth/user-profile")
+    fun getProfile(
+        @Header("Authorization") authHeader: String?,
+    ): Response<User>
+
+    @FormUrlEncoded
+    @POST("user/update-data")
+    fun update_data(
+        @Header("Authorization") token: String,
+        @Field("user_email") email: String,
+        @Field("user_role") role: String,
+        @Field("user_name") name: String,
+        @Field("user_contact") contact: String,
+    ): Call<UserUpdateProfileResponse>
+
+
+    /**
+     * register
+     * @param register
+     * @return the token
+     */
+    @POST("auth/register")
+    fun register(
+        @Header("Authorization") authHeader: String?,
+        @Body body: RegisterRequestBody?,
+    ): Call<ResponseBody>?
+
+    /**
+     * register
+     *
+     * change password
+     * @param token,old_password,new_password
+     * @return the token
+     */
+    @FormUrlEncoded
+    @POST("user/change-password")
+    fun changePassword(
+        @Header("Authorization") token: String?,
+        @Field("old_password") old_password: String,
+        @Field("new_password") new_password: String,
+    ): Call<ChangePasswordResponse>?
+
+    /**
+     * news
+     * @param get news data
+     * @return the token
+     */
+    @GET("news/get")
+    suspend fun getNews(
+        @Header("Authorization") authHeader: String?,
+    ): Response<NewsResponse?>
+
+
+
 }
