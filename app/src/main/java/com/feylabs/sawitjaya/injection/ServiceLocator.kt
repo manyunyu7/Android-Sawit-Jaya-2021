e2364c79f623747767a9e28b23eb4cb8d60c2996
henryaugusta <henryaugusta4@gmail.com>
2021-09-01 20:46:12 +0700

    🧰 Change ServiceLocator.kt because of new dependencies

--- app/src/main/java/com/feylabs/sawitjaya/injection/ServiceLocator.kt
+++ app/src/main/java/com/feylabs/sawitjaya/injection/ServiceLocator.kt
@@ -3,26 +3,45 @@ package com.feylabs.sawitjaya.injection
 import android.content.Context
 import com.feylabs.sawitjaya.ViewModelFactory
 import com.feylabs.sawitjaya.data.AuthRepository
+import com.feylabs.sawitjaya.data.SawitRepository
+import com.feylabs.sawitjaya.data.local.LocalDataSource
+import com.feylabs.sawitjaya.data.local.preference.MyPreference
+import com.feylabs.sawitjaya.data.local.room.MyRoomDatabase
 import com.feylabs.sawitjaya.data.remote.RemoteDataSource
 import com.feylabs.sawitjaya.service.ApiService
-import com.feylabs.sawitjaya.service.MainEndpoint
 
 object ServiceLocator {
 
-     fun provideAuthRepository(context: Context) : AuthRepository{
+    fun provideAuthRepository(context: Context): AuthRepository {
         val apiService = ApiService.getClient(context)
-        val remoteDataSource = RemoteDataSource(apiService)
-        val authRepository = AuthRepository(remoteDataSource)
+        val remoteDataSource = RemoteDataSource(apiService,context)
+        val roomDb = MyRoomDatabase.invoke(context)
+        val localDataSource = LocalDataSource(roomDb, MyPreference(context))
+        val authRepository = AuthRepository(remoteDataSource, localDataSource)
 
         return authRepository
     }
 
+    fun provideSawitRepository(context: Context): SawitRepository {
+        val apiService = ApiService.getClient(context)
+        val remoteDataSource = RemoteDataSource(apiService,context)
+        val roomDb = MyRoomDatabase.invoke(context)
+        val localDataSource = LocalDataSource(roomDb, MyPreference(context))
+        val repo = SawitRepository(remoteDataSource, localDataSource)
+
+        return repo
+    }
+
+
     fun provideFactory(context: Context): ViewModelFactory {
         val apiService = ApiService.getClient(context)
-        val remoteDataSource = RemoteDataSource(apiService)
-        val authRepository = AuthRepository(remoteDataSource)
+        val remoteDataSource = RemoteDataSource(apiService,context)
+        val roomDb = MyRoomDatabase.invoke(context)
+        val localDataSource = LocalDataSource(roomDb, MyPreference(context))
+        val authRepository = AuthRepository(remoteDataSource, localDataSource)
+        val sawitRepository = SawitRepository(remoteDataSource, localDataSource)
 
-        return ViewModelFactory(authRepository)
+        return ViewModelFactory(sawitRepository,authRepository)
     }
 
 
