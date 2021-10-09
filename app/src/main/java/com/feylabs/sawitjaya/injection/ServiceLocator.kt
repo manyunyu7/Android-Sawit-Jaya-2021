d9458b58012b4596cca89d88cef81ce0884851c6
Henry Augusta <henryaugusta4@gmail.com>
2021-10-09 23:55:37 +0700

    Add new URL to ServiceLocator.kt

--- app/src/main/java/com/feylabs/sawitjaya/injection/ServiceLocator.kt
+++ app/src/main/java/com/feylabs/sawitjaya/injection/ServiceLocator.kt
@@ -11,11 +11,12 @@ import com.feylabs.sawitjaya.utils.service.ApiClient
 
 object ServiceLocator {
 
-    const val BASE_URL = "http://192.168.1.161:3202/api/"
+    const val REAL_URL = "http://192.168.1.161:3202/"
+    const val BASE_URL = "${REAL_URL}api/"
 
     fun provideAuthRepository(context: Context): AuthRepository {
         val apiService = ApiClient.getClient(context)
-        val remoteDataSource = RemoteDataSource(apiService,context)
+        val remoteDataSource = RemoteDataSource(apiService, context)
         val roomDb = MyRoomDatabase.invoke(context)
         val localDataSource = LocalDataSource(roomDb)
         val authRepository = AuthRepository(remoteDataSource, localDataSource)
@@ -25,7 +26,7 @@ object ServiceLocator {
 
     fun provideSawitRepository(context: Context): SawitRepository {
         val apiService = ApiClient.getClient(context)
-        val remoteDataSource = RemoteDataSource(apiService,context)
+        val remoteDataSource = RemoteDataSource(apiService, context)
         val roomDb = MyRoomDatabase.invoke(context)
         val localDataSource = LocalDataSource(roomDb)
         val repo = SawitRepository(remoteDataSource, localDataSource)
@@ -36,13 +37,13 @@ object ServiceLocator {
 
     fun provideFactory(context: Context): ViewModelFactory {
         val apiService = ApiClient.getClient(context)
-        val remoteDataSource = RemoteDataSource(apiService,context)
+        val remoteDataSource = RemoteDataSource(apiService, context)
         val roomDb = MyRoomDatabase.invoke(context)
         val localDataSource = LocalDataSource(roomDb)
         val authRepository = AuthRepository(remoteDataSource, localDataSource)
         val sawitRepository = SawitRepository(remoteDataSource, localDataSource)
 
-        return ViewModelFactory(sawitRepository,authRepository)
+        return ViewModelFactory(sawitRepository, authRepository)
     }
 
 
